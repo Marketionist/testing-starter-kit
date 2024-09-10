9dd4c194da849ec93b50e90c9a7910ceddd98692
Marketionist <marketionist@gmail.com>
2024-09-10 08:11:43 -0400

    Update links test

--- tests/links.test.js
+++ tests/links.test.js
@@ -1,11 +1,21 @@
 const { checkFile } = require('img-link-checker');
 
-checkFile(
-    './README.md',
-    [
-        'https://twitter.com/mpjme',
-        'https://codepen.io/',
-        'https://stackoverflow.com/questions/19389131/how-to-do-multiple-user-logins-with-different-inputs-in-jmeter',
-        'https://www.toptal.com/puppeteer/headless-browser-puppeteer-tutorial'
-    ]
-);
+(async () => {
+    const brokenLinks = await checkFile(
+        './README.md',
+        /* eslint-disable max-len */
+        [
+            'https://twitter.com/mpjme',
+            'https://codepen.io/',
+            'https://stackoverflow.com/questions/19389131/how-to-do-multiple-user-logins-with-different-inputs-in-jmeter',
+            'https://www.toptal.com/puppeteer/headless-browser-puppeteer-tutorial'
+        ]
+        /* eslint-enable max-len */
+    );
+
+    // Print a list of broken links and exit the process (will mark the CI/CD build as failed)
+    if (brokenLinks.length > 0) {
+        console.error('Broken links list:', brokenLinks);
+        process.exit(1);
+    }
+})();
