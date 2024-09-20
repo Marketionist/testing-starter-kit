25c90f6f3d89cfb02bf41b99d7087aaebd943edb
Marketionist <marketionist@gmail.com>
2024-09-20 08:52:36 -0400

    Ignore checking gnu.org to avoid 429 errors

--- tests/links.test.js
+++ tests/links.test.js
@@ -8,7 +8,9 @@ const { checkFile } = require('img-link-checker');
             'https://twitter.com/mpjme',
             'https://codepen.io/',
             'https://stackoverflow.com/questions/19389131/how-to-do-multiple-user-logins-with-different-inputs-in-jmeter',
-            'https://www.toptal.com/puppeteer/headless-browser-puppeteer-tutorial'
+            'https://www.toptal.com/puppeteer/headless-browser-puppeteer-tutorial',
+            'https://www.gnu.org/savannah-checkouts/gnu/grep/manual/grep.html',
+            'https://www.gnu.org/software/gawk/manual/gawk.html'
         ]
         /* eslint-enable max-len */
     );
