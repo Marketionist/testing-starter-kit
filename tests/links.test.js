5068877846b7e705ec601046306e96d400fc1db8
Marketionist <marketionist@gmail.com>
2025-07-04 08:55:44 -0400

    Fix tests

--- tests/links.test.js
+++ tests/links.test.js
@@ -10,7 +10,21 @@ const { checkFile } = require('img-link-checker');
             'https://stackoverflow.com/questions/19389131/how-to-do-multiple-user-logins-with-different-inputs-in-jmeter',
             'https://www.toptal.com/puppeteer/headless-browser-puppeteer-tutorial',
             'https://www.gnu.org/savannah-checkouts/gnu/grep/manual/grep.html',
-            'https://www.gnu.org/software/gawk/manual/gawk.html'
+            'https://www.gnu.org/software/gawk/manual/gawk.html',
+            'http://blog.getpostman.com/2017/10/25/writing-tests-in-postman/',
+            'https://medium.com/@dmitriev.victor/creating-postman-tests-that-scale-2-5-d71ad0221d2a',
+            'https://medium.com/@dmitriev.victor/creating-postman-tests-that-scale-3-5-67e3565dd069',
+            'https://medium.com/@dmitriev.victor/creating-postman-tests-that-scale-4-5-ae8aaaac58f7',
+            'https://medium.com/@dmitriev.victor/creating-postman-tests-that-scale-6ac92c514750',
+            'https://medium.com/mop-developers/testing-your-api-with-dredd-c02e6ca151f2',
+            'https://medium.com/@me_37286/yoni-goldberg-javascript-nodejs-testing-best-practices-2b98924c9347',
+            'https://medium.com/@ehmicky/execa-9-release-d0d5daaa097f',
+            'https://medium.com/@me_37286/yoni-goldberg-javascript-nodejs-testing-best-practices-2b98924c9347',
+            'https://medium.com/dailyjs/the-pitfalls-of-async-await-in-array-loops-cf9cf713bfeb',
+            'https://medium.com/dailyjs/how-to-use-npm-link-7375b6219557',
+            'https://medium.com/hackernoon/on-r%C3%A9sum%C3%A9s-and-applying-to-jobs-4c8cfcc7bf26',
+            'https://davidkcaudill.medium.com/maybe-getting-rid-of-your-qa-team-was-bad-actually-52c408bd048b',
+            'http://www.qarevolution.com/one-key-metric-should-drive-quality-engineering/'
         ]
         /* eslint-enable max-len */
     );
